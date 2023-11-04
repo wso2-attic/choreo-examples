e4f894b649e55279a8dfff7d69905d44eab8f2ae
ThumulaPerera <42399179+ThumulaPerera@users.noreply.github.com>
2023-11-04 18:18:41 +0530

    Fix org name claim

--- cloud-native-app-developer/reading-list-front-end/src/index.tsx
+++ cloud-native-app-developer/reading-list-front-end/src/index.tsx
@@ -122,7 +122,7 @@ export default function App() {
           <div className="flex justify-between items-center">
             {user && (
               <a href="#" className="font-bold text-xl text-[#36d1dc]">
-                {user?.orgName}
+                {user?.org_name}
               </a>
             )}
             <button
