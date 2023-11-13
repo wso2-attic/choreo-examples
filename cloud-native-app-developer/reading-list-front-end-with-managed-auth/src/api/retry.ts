c779e056a392660df44cfde16c36d437da512659
ThumulaPerera <42399179+ThumulaPerera@users.noreply.github.com>
2023-11-13 11:02:01 +0530

    Fix token refresh

--- cloud-native-app-developer/reading-list-front-end-with-managed-auth/src/api/retry.ts
+++ cloud-native-app-developer/reading-list-front-end-with-managed-auth/src/api/retry.ts
@@ -32,7 +32,7 @@ export const performRequestWithRetry = async (url: string, options: AxiosRequest
         const retryResponse = await axios(url, options);
         return retryResponse;
       } catch (refreshError) {
-        if (error.response && error.response.status === 401) {
+        if (refreshError.response && refreshError.response.status === 401) {
           // Session has expired (i.e., Refresh token has also expired).
           // Redirect to the login page
           console.log('Failed to refresh token. Status: ' + refreshError.response.status);
@@ -40,7 +40,7 @@ export const performRequestWithRetry = async (url: string, options: AxiosRequest
         } else {
           // We can't refresh the token due to a server error.
           // Hence just throw the original 401 error from the API.
-          throw refreshError;
+          throw error;
         }
       }
     } else {
